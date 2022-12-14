21edd8969c992762a8847862b822b9052851cfdc
atuok-git <kouta.fukumoto7221@gmail.com>
2022-12-15 00:43:28 +0900

    第7章

--- laravel/app/Article.php
+++ laravel/app/Article.php
@@ -2,9 +2,12 @@
 
 namespace App;
 
+use Dotenv\Lines;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Foundation\Auth\User;
+use Psy\CodeCleaner\FunctionReturnInWriteContextPass;
 
 class Article extends Model
 {
@@ -16,5 +19,22 @@ class Article extends Model
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function likes(): BelongsToMany
+    {
+        return $this->belongsToMany(User::class, 'likes')->withTimestamps();
+    }
+
+    public function isLikedBy(?User $user): bool
+    {
+        return $user
+        ? (bool)$this->likes->where('id', $user->id)->count()
+        : false;
+    }
+
+    public function getCountLikesAttribute(): int
+    {
+        return $this->likes->count();
     }
 }
